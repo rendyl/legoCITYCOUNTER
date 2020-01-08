c16d366acf38bf685c49627767b4514a256ed740
antoinechedin <antoine.chedin@gmail.com>
2020-01-08 14:57:25 +0100

    Add 4 Bordeaux tiles

---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    GameObject[] tiles;
+
+
+    private void Start()
+    {
+
+
+    }
+
+    private void GatherTiles()
+    {
+        tiles = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            tiles[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+}
