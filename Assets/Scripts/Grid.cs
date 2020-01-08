0097a647a4dfac6a5107562c9eb1e3ac784e66a6
Rendyl <r.latbi@laposte.net>
2020-01-08 16:13:00 +0100

    Grid and RayCast

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public Vector3 sizeGrid;
+    public int sizeVoxel;
+    public Vector3 basePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = new Color(1, 0, 0, 0.75F);
+
+        if(sizeVoxel != 0)
+        {
+            for (int i = 0; i <= sizeGrid.x / sizeVoxel; i++)
+            {
+
+                for (int j = 0; j <= sizeGrid.y / sizeVoxel; j++)
+                {
+                    Gizmos.DrawLine(transform.position + basePos + new Vector3(i, j, 0) * sizeVoxel, transform.position + basePos + new Vector3(i, j, sizeGrid.z / sizeVoxel) * sizeVoxel);
+                }
+            }
+
+            for (int j = 0; j <= sizeGrid.y / sizeVoxel; j++)
+            {
+                for (int k = 0; k <= sizeGrid.z / sizeVoxel; k++)
+                {
+                    Gizmos.DrawLine(transform.position + basePos + new Vector3(0, j, k) * sizeVoxel, transform.position + basePos + new Vector3(sizeGrid.x / sizeVoxel, j, k) * sizeVoxel);
+                    //Gizmos.DrawSphere(transform.position + basePos + new Vector3(i, j, k) * sizeVoxel, 1);
+                }
+            }
+
+            for (int i = 0; i <= sizeGrid.x / sizeVoxel; i++)
+            {
+                for (int k = 0; k <= sizeGrid.z / sizeVoxel; k++)
+                {
+                    Gizmos.DrawLine(transform.position + basePos + new Vector3(i, 0, k) * sizeVoxel, transform.position + basePos + new Vector3(i, sizeGrid.y / sizeVoxel, k) * sizeVoxel);
+                    //Gizmos.DrawSphere(transform.position + basePos + new Vector3(i, j, k) * sizeVoxel, 1);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RayCast.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCast : MonoBehaviour
+{
+    public Grid g;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(0, 1, 0, 0.75F);
+
+        for (int i = 1; i <= g.sizeGrid.x / g.sizeVoxel; i++)
+        {
+            for (int k = 1; k <= g.sizeGrid.z / g.sizeVoxel; k++)
+            {
+                Gizmos.DrawLine(transform.position + g.basePos + new Vector3(-g.sizeVoxel / 2, 0, -g.sizeVoxel / 2) + new Vector3(i, g.sizeGrid.y / g.sizeVoxel, k) * g.sizeVoxel, transform.position + g.basePos + new Vector3(-g.sizeVoxel / 2, 0, -g.sizeVoxel / 2) + new Vector3(i, 0, k) * g.sizeVoxel);
+            }
+        }
+    }
+}
