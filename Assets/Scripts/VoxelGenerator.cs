8396b2365705ebab7f970fa2747715435fb06016
antoinechedin <antoine.chedin@gmail.com>
2020-01-10 11:27:35 +0100

    Finish map generation

--- Assets/Scripts/VoxelGenerator.cs
+++ Assets/Scripts/VoxelGenerator.cs
@@ -28,10 +28,13 @@ public class VoxelGenerator : MonoBehaviour
         meshRenderer.material = new Material(Shader.Find("Standard"));
 
         Mesh voxelMesh = new Mesh();
+        voxelMesh.indexFormat = UnityEngine.Rendering.IndexFormat.UInt32;
         meshColumns = CreateVoxelColumnsFromJson(jsonFile.text);
         voxelMesh.vertices = GetVertices(meshColumns);
         voxelMesh.triangles = GetTriangles(meshColumns);
         voxelMesh.RecalculateNormals();
+        voxelMesh.RecalculateBounds();
+        voxelMesh.RecalculateTangents();
 
         meshFilter.mesh = voxelMesh;
     }
@@ -58,12 +61,14 @@ public class VoxelGenerator : MonoBehaviour
 
     private static Vector3[] GetVertices(MeshColumn[,] meshColumns)
     {
-        Vector3[] vertices = new Vector3[meshColumns.Length * 4];
+        Vector3[] vertices = new Vector3[meshColumns.Length * 8];
+        int sideOffset = meshColumns.Length * 4;
         foreach (MeshColumn meshColumn in meshColumns)
         {
             for (int i = 0; i < 4; i++)
             {
                 vertices[meshColumn.id * 4 + i] = meshColumn.vertices[i];
+                vertices[sideOffset + meshColumn.id * 4 + i] = meshColumn.vertices[4 + i];
             }
         }
 
@@ -72,17 +77,74 @@ public class VoxelGenerator : MonoBehaviour
 
     private static int[] GetTriangles(MeshColumn[,] meshColumns)
     {
-        int[] triangles = new int[meshColumns.Length * 6];
+        int topCount = meshColumns.Length * 2;
+        int sideCount = (4 * meshColumns.Length - 2 * (meshColumns.GetLength(0) + meshColumns.GetLength(1)));
 
-        foreach (MeshColumn meshColumn in meshColumns)
+        int sideId = topCount * 3;
+        int sideOffset = meshColumns.Length * 4;
+
+        int[] triangles = new int[3 * (topCount + sideCount)];
+
+        for (int x = 0; x < meshColumns.GetLength(0); x++)
         {
-            int offset = meshColumn.id * 4;
-            triangles[meshColumn.id * 6] = offset;
-            triangles[meshColumn.id * 6 + 1] = offset + 1;
-            triangles[meshColumn.id * 6 + 2] = offset + 2;
-            triangles[meshColumn.id * 6 + 3] = offset;
-            triangles[meshColumn.id * 6 + 4] = offset + 2;
-            triangles[meshColumn.id * 6 + 5] = offset + 3;
+            for (int y = 0; y < meshColumns.GetLength(1); y++)
+            {
+                MeshColumn meshColumn = meshColumns[x, y];
+                int offset = meshColumn.id * 4;
+                // Top mesh
+                triangles[meshColumn.id * 6] = offset;
+                triangles[meshColumn.id * 6 + 1] = offset + 1;
+                triangles[meshColumn.id * 6 + 2] = offset + 2;
+                triangles[meshColumn.id * 6 + 3] = offset;
+                triangles[meshColumn.id * 6 + 4] = offset + 2;
+                triangles[meshColumn.id * 6 + 5] = offset + 3;
+
+                // Side mesh
+                if (x != 0)
+                {
+                    if (meshColumns[x, y].GetHeight() > meshColumns[x - 1, y].GetHeight())
+                    {
+                        triangles[sideId] =  sideOffset + meshColumns[x - 1, y].id * 4 + 2;
+                        triangles[sideId + 1] = sideOffset + meshColumns[x, y].id * 4 + 1;
+                        triangles[sideId + 2] = sideOffset + meshColumns[x, y].id * 4;
+                        triangles[sideId + 3] = sideOffset + meshColumns[x - 1, y].id * 4 + 2;
+                        triangles[sideId + 4] = sideOffset + meshColumns[x, y].id * 4;
+                        triangles[sideId + 5] = sideOffset + meshColumns[x - 1, y].id * 4 + 3;
+                    }
+                    else
+                    {
+                        triangles[sideId] = sideOffset + meshColumns[x, y].id * 4;
+                        triangles[sideId + 1] = sideOffset + meshColumns[x - 1, y].id * 4 + 3;
+                        triangles[sideId + 2] = sideOffset + meshColumns[x - 1, y].id * 4 + 2;
+                        triangles[sideId + 3] = sideOffset + meshColumns[x, y].id * 4;
+                        triangles[sideId + 4] = sideOffset + meshColumns[x - 1, y].id * 4 + 2;
+                        triangles[sideId + 5] = sideOffset + meshColumns[x, y].id * 4 + 1;
+                    }
+                    sideId += 6;
+                }
+                if (y != 0)
+                {
+                    if (meshColumns[x, y].GetHeight() > meshColumns[x, y - 1].GetHeight())
+                    {
+                        triangles[sideId] = sideOffset + meshColumns[x, y - 1].id * 4 + 1;
+                        triangles[sideId + 1] = sideOffset + meshColumns[x, y].id * 4;
+                        triangles[sideId + 2] = sideOffset + meshColumns[x, y].id * 4 + 3;
+                        triangles[sideId + 3] = sideOffset + meshColumns[x, y - 1].id * 4 + 1;
+                        triangles[sideId + 4] = sideOffset + meshColumns[x, y].id * 4 + 3;
+                        triangles[sideId + 5] = sideOffset + meshColumns[x, y - 1].id * 4 + 2;
+                    }
+                    else
+                    {
+                        triangles[sideId] = sideOffset + meshColumns[x, y].id * 4 + 3;
+                        triangles[sideId + 1] = sideOffset + meshColumns[x, y - 1].id * 4 + 2;
+                        triangles[sideId + 2] = sideOffset + meshColumns[x, y - 1].id * 4 + 1;
+                        triangles[sideId + 3] = sideOffset + meshColumns[x, y].id * 4 + 3;
+                        triangles[sideId + 4] = sideOffset + meshColumns[x, y - 1].id * 4 + 1;
+                        triangles[sideId + 5] = sideOffset + meshColumns[x, y].id * 4;
+                    }
+                    sideId += 6;
+                }
+            }
         }
 
         return triangles;
@@ -98,11 +160,15 @@ public class MeshColumn
     public MeshColumn(int id, Vector2 position, float height, Column.Type type)
     {
         this.id = id;
-        this.vertices = new Vector3[4];
+        this.vertices = new Vector3[8];
         this.vertices[0] = new Vector3(position.x, height, position.y);
         this.vertices[1] = new Vector3(position.x, height, position.y + 1f);
         this.vertices[2] = new Vector3(position.x + 1f, height, position.y + 1f);
         this.vertices[3] = new Vector3(position.x + 1f, height, position.y);
+        this.vertices[4] = this.vertices[0];
+        this.vertices[5] = this.vertices[1];
+        this.vertices[6] = this.vertices[2];
+        this.vertices[7] = this.vertices[3];
         this.type = type;
     }
 
@@ -112,5 +178,10 @@ public class MeshColumn
         {
             vertices[i].y = value;
         }
+    }
+
+    public float GetHeight()
+    {
+        return vertices[0].y;
     }
 }
